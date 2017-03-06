27aec39db2f909f4af7aebd2bbbf18c69fc84f65
mmendesc <matheus.mendescf@gmail.com>
2017-03-05 21:48:04 -0300

    Ball attaches to players

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private GameObject player;
+	private bool isCarried;
+	private int activePlayer;
+	// Use this for initialization
+	void Start () {
+		activePlayer = PlayerController.activePlayer + 1;
+		player = GameObject.Find(string.Concat("Player", activePlayer));
+		isCarried = false;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		activePlayer = PlayerController.activePlayer + 1;
+		player = GameObject.Find(string.Concat("Player", activePlayer));
+
+
+		if (Input.GetKeyDown(KeyCode.B)) {
+			isCarried = true;
+			this.transform.position = player.transform.position+ new Vector3(0.309f,0,0);
+		}
+
+		if (isCarried) { 
+			this.transform.position = player.transform.position + new Vector3(0.309f, 0, 0);
+		}
+	}
+}
